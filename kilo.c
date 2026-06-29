bc410f14c1526529221dd4d5f4ef65311ec65941
ahmedwahbasw <42310239.Ahmed@acu.edu.eg>
2026-06-29 13:01:55 +0300

    added extra flags

--- kilo.c
+++ kilo.c
@@ -17,8 +17,8 @@ void enableRawMode(){
    
 
    tcgetattr(STDIN_FILENO, &raw);
-   raw.c_iflag &=  ~(IXON);
-   raw.c_lflag &=  ~(ECHO | ICANON|ISIG);
+   raw.c_iflag &=  ~(ICRNL | IXON);
+   raw.c_lflag &=  ~(ECHO | ICANON|IEXTEN|ISIG);
    tcsetattr(STDIN_FILENO, TCSAFLUSH , &raw);
 }
 
